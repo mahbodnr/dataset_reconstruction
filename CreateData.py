1647cd412398251d85d0e1b2d63fb552a6019f71
Mahbod Nouri <mahbodnouri@gmail.com>
2023-05-14 19:08:50 +0200

    debug create data

--- CreateData.py
+++ CreateData.py
@@ -4,19 +4,15 @@ def setup_problem(args):
         pass
     elif args.problem == 'cifar10_vehicles_animals':
         from problems.cifar10_vehicles_animals import get_dataloader
-        return get_dataloader(args)
     elif args.problem == 'mnist_odd_even':
         from problems.mnist_odd_even import get_dataloader
-        return get_dataloader(args)
     elif args.problem == 'mnist_odd_even_noisy_background':
         from problems.mnist_odd_even_noisy_background import get_dataloader
-        return get_dataloader(args)
     elif args.problem == 'cifar10_noisy_background':
         from problems.cifar10_noisy_background import get_dataloader
-        return get_dataloader(args)
     elif args.problem == 'tiny_imagenet':
         from problems.tiny_imagenet import get_dataloader
     else:
         raise ValueError(f'Unknown args.problem={args.problem}')
-    return data_loader
+    return get_dataloader(args)
 
