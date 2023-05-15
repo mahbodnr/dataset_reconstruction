e6ba9879954c7333a592618de5ea6f4416aca855
Mahbod Nouri <mahbodnouri@gmail.com>
2023-05-15 08:45:37 +0200

    add noisy tiny imagenet

--- problems/tiny_imagenet_noisy_background.py
+++ problems/tiny_imagenet_noisy_background.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision.datasets as datasets
 import torchvision.transforms
+import torchvision.transforms.functional as F
+import random
+
 
 
 def load_bound_dataset(dataset, batch_size, shuffle=False, start=None, end=None, **kwargs):
@@ -15,7 +18,7 @@ def load_bound_dataset(dataset, batch_size, shuffle=False, start=None, end=None,
     return torch.utils.data.DataLoader(dataset, batch_size, shuffle=shuffle, **kwargs)
 
 
-def fetch_tiny_imagenet(root, train, transform=None, target_transform=None):
+def fetch_tiny_imagenet(root, train=False, transform=None, target_transform=None):
     transform = transform if transform is not None else torchvision.transforms.ToTensor()
     if train:
         root+= "/train"
@@ -46,7 +49,7 @@ def create_labels(y0):
     return y0
 
 
-def get_balanced_data(args, data_loader, data_amount):
+def get_balanced_data(args, data_loader, data_amount, train):
     print('BALANCING DATASET...')
     # get balanced data
     data_amount_per_class = data_amount // 2
@@ -67,20 +70,57 @@ def get_balanced_data(args, data_loader, data_amount):
         if got_enough:
             break
     x0, y0 = torch.stack(x0), torch.stack(y0)
+
+    if train == True:
+        n_images = y0.shape[0]
+        if args.bias_type == 'square':
+            # n_images = y0.shape[0]
+            n_noisy_images = int(args.noise_perc*n_images)
+            if args.noise_mode == 'fixed_squares':
+                for ii in range(n_noisy_images):
+                    if y0[ii] == 0:
+                        x0[ii, :, 0:3, 0:3] = 0
+
+                    elif y0[ii] == 1:
+                        x0[ii, :, 0:3, 0:3] = 1
+            elif args.noise_mode == 'non_fixed_squares':
+                random.seed(40)
+                clue_pos = random.sample(range(1, 29), 20)
+                class1_pos = clue_pos[2:7]
+                print('class 1 clue positions: ', class1_pos)
+                class2_pos = clue_pos[10:15]
+                print('class 2 clue positions: ', class2_pos)
+                for ii in range(n_noisy_images):
+                    if y0[ii] == 0:
+                        pos = class1_pos[ii % 5]
+                        x0[ii, :, pos:pos+3, pos:pos+3] = 0
+
+                    elif y0[ii] == 1:
+                        pos = class2_pos[ii % 5]
+                        x0[ii, :, pos:pos+3, pos:pos+3] = 1
+
+        elif args.bias_type == 'contrast':
+            for ii in range(n_images):
+                    if y0[ii] == 0:
+                        x0[ii] = F.adjust_contrast(x0[ii], args.contrast_factor_1)
+                    elif y0[ii] == 1:
+                        x0[ii] = F.adjust_contrast(x0[ii], args.contrast_factor_2)   
+
     return x0, y0
+
 
 
 def load_tiny_imagenet_data(args):
     # Get Train Set
     print('TRAINSET BALANCED')
     data_loader = load_tiny_imagenet(root=args.datasets_dir, batch_size=100, train=True, shuffle=False, start=0, end=50000)
-    x0, y0 = get_balanced_data(args, data_loader, args.data_amount)
+    x0, y0 = get_balanced_data(args, data_loader, args.data_amount, train=True)
 
     # Get Test Set (balanced)
     print('LOADING TESTSET')
     assert not args.data_use_test or (args.data_use_test and args.data_test_amount >= 2), f"args.data_use_test={args.data_use_test} but args.data_test_amount={args.data_test_amount}"
     data_loader = load_tiny_imagenet(root=args.datasets_dir, batch_size=100, train=False, shuffle=False, start=0, end=10000)
-    x0_test, y0_test = get_balanced_data(args, data_loader, args.data_test_amount)
+    x0_test, y0_test = get_balanced_data(args, data_loader, args.data_test_amount, train=False)
 
     # move to cuda and double
     x0, y0 = move_to_type_device(x0, y0, args.device)
